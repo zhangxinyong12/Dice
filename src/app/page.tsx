be5b251ffdd64843cbd214cd9c4941d3405a5796
张新永 <xinyong.zhang@zen-x.com.cn>
2023-09-11 18:10:26 +0800

    feat: 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,190 @@
-import Image from 'next/image'
+"use client"
+
+import Image from "next/image"
+import { useState, useRef, useEffect } from "react"
 
 export default function Home() {
+  const [a] = useState<number>(1)
+  const domRef = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      rootMargin: "0px",
+      threshold: 0.1, // 根据需要调整此值
+    })
+
+    if (domRef.current) {
+      observer.observe(domRef.current)
+    }
+
+    return () => {
+      if (domRef.current) {
+        observer.unobserve(domRef.current)
+      }
+    }
+  }, [])
+
+  function handleIntersection(entries) {
+    if (entries[0].isIntersecting) {
+      console.log("intersecting")
+      console.log("元素可见")
+    }
+    console.log(entries)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="p-4">
+      <div className="sticky top-0 bg-black">头部吸顶</div>
+      <div className="first:ml-4 last:ml-40">
+        <button className="bg-sky-500 hover:bg-sky-700">save changes</button>
+        <button
+          className="bg-violet-500 ml-2 py-10 px-14
+        active:bg-violet-700
+        focus:bg-violet-200
+      hover:bg-violet-600"
+        >
+          save changes
+        </button>
+        <span
+          className="block before:content-['*']
+        before:mr-1
+        before:font-sans
+        before:text-red-500"
+        >
+          block
+        </span>
+        <ul
+          role={"list"}
+          className="marker:text-sky-500 list-disc text-slate-500"
+        >
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+          <li>1</li>
+        </ul>
+        <div className="w-96">
+          <p className="text-blue-600 selection:bg-fuchsia-500 selection:text-red-400">
+            于是我开始下水。我不会骗你们男孩，我很害怕。但我继续前进，当我走过破碎机时，一种奇怪的平静笼罩着我。我不知道这是上帝的干预还是所有生物的亲属关系，但我告诉你，杰瑞，那一刻，我是一名海洋生物学家。
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-60 first-line  first-letter:text-7xl first-letter:text-gray-400">
+        well, let me tell you something, funny boy. Y'know that little stamp,
+        the one that says "New York Public Library"? Well that may not mean
+        anything to you, but that means a lot to me. One whole hell of a lot.
+      </div>
+      <div className="px-[10px] py-[16px] border-2  text-center  w-[140px] text-[24px] bg-gray-500 text-[#33ff00]">
+        140px
+      </div>
+      <div>
+        <div className="container mx-auto px-4 bg-red-300 text-center">
+          水平居中
+        </div>
+      </div>
+      <div className="columns-4 gap-20">
+        <div className="hover:bg-red-600 w-full m-4 bg-blue-200 ">1</div>
+        <div className="w-full m-4 bg-blue-200 ">1</div>
+        <div className="w-full m-4 bg-blue-200 ">1</div>
+        <div className="w-full m-4 bg-blue-200 ">1</div>
+        <div className="w-full m-4 bg-blue-200 ">1</div>
+        <div className="w-full m-4 bg-blue-200 ">1</div>
+        <div className="w-full m-4 bg-blue-200 ">1</div>
+        <div className="w-full m-4 bg-blue-200 ">1</div>
+      </div>
+      <div>
+        <span className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ">
+          Hello
+          <br />
+          World
+        </span>
+        <span className="box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ">
+          Hello
+          <br />
+          World
+        </span>
+      </div>
+      <div className="box-border bg-gray-100 h-[40px] w-[120px] p-[10px] border-[1px] border-red-500 hover:box-content">
+        2
+      </div>
+      <div className="header mt-[10px] w-full flex h-[60] bg-gray-100 justify-between">
+        {[
+          {
+            id: 1,
+            name: "首页",
+          },
+          {
+            id: 2,
+            name: "首页2",
+          },
+          {
+            id: 3,
+            name: "首页3",
+          },
+        ].map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="bg-[#3366ff] w-[120px] h-[60px] leading-[60px] text-center hover:bg-[#ff3366] cursor-pointer text-[#fff] text-[14px] font-bold rounded-md"
+            >
+              {item.name}
+            </div>
+          )
+        })}
+      </div>
+      <div className="test-float h-[20px] mt-[40px]">
+        {[1, 2, 3, 4].map((el) => {
+          return (
+            <div className="float-left h-[20px]" key={el}>
+              {el}
+            </div>
+          )
+        })}
+      </div>
+      <div>12222222</div>
+      <div className="truncate">
+        在上面的示例中，我们给一个包含文本的元素添加了 w-32
+        类，该类设置了元素的宽度（这里是32个字符的宽度）。然后，我们添加了
+        truncate 类，以指示文本溢出时应该显示省略号。
+        你可以根据需要调整容器的宽度和其他样式类，以满足你的设计要求。当文本内容超过容器宽度时，文本将被截断并显示省略号，以防止溢出破坏布局。
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="line-clamp-3 ">
+        在上面的示例中，我们给一个包含文本的元素添加了 w-32
+        类，该类设置了元素的宽度（这里是32个字符的宽度）。然后，我们添加了
+        truncate 类，以指们给一个包含文本的元素添加了 w-32
+        类，该类设置了元素的宽度（这里是32个字符的宽度）。然后，我们添加了
+        truncate 类，以指示文本溢出时应该显示省略号。
+        你可以根据需要调整容们给一个包含文本的元素添加了 w-32
+        类，该类设置了元素的宽度（这里是32个字符的宽度）。然后，我们添加了
+        truncate 类，以指示文本溢出时应该显示省略号。
+        你可以根据需要调整容示文本溢出时应该显示省略号。
+        你可以根据需要调整容器的宽度和其他样式类，以满足你的设计要求。当文本内容超过容器宽度时，文本将被截断并显示省略号，以防止溢出破坏布局。
       </div>
+      <div className="w-full h-[250px] bg-purple-100"></div>
+      <div className="relative h-[50px] bg-[#333]">
+        <span className="absolute text-white left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
+          水平 垂直 居中
+        </span>
+      </div>
+      <div className="relative h-[250px] w-[400px] border-red-500  border-[10px]">
+        <span className="bg-[#999] absolute right-0 bottom-0"> x - 30px</span>
+      </div>
+      <div className="flex flex-wrap items-center">
+        {[1, 2, 3, 4, 2, 3, 4, 2].map((el, index) => {
+          return (
+            <div
+              className={
+                el % 2 === 0 ? "basis-1/4 bg-red-500" : "basis-1/2 bg-blue-500"
+              }
+              key={el + "_" + index}
+            >
+              {el}
+            </div>
+          )
+        })}
+      </div>
+      <div ref={domRef}>观察对象</div>
     </main>
   )
 }
---
+++ tailwindcss.md
@@ -0,0 +1,34 @@
+# tailwindcss 学习笔记
+## rem
+1rem = 16px    
+mt-1= margin-top: 0.25rem; 1=0.25rem    
+水平方向 x   
+垂直方向 y 
+以上只有偶数，没有奇数
+## color
+[官网文档地址](https://tailwindcss.com/docs/customizing-colors)
+以下是提取的颜色单词的翻译
+- slate - 蓝灰色
+- gray - 灰色
+- zinc - 锌色
+- neutral - 中性色
+- stone - 石色
+- red - 红色
+- orange - 橙色
+- amber - 琥珀色
+- yellow - 黄色
+- lime - 酸橙色
+- green - 绿色
+- emerald - 翠绿色
+- teal - 鸭翅绿色
+- cyan - 青色
+- sky - 天空蓝色
+- blue - 蓝色
+- indigo - 靛蓝色
+- violet - 紫罗兰色
+- purple - 紫色
+- fuchsia - 紫红色
+- pink - 粉红色
+- rose - 玫瑰色
+
+这些单词代表了不同的颜色系列，你可以在Tailwind CSS中使用它们来设置文本颜色、背景颜色、边框颜色等。
